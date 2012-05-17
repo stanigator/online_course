0375dc0e47f4721a63e9eb160b3d5d678efb7c17
Stanley Lee <stanigator@gmail.com>
2012-05-17 00:38:40 -0700

    Added files relevant for the JSON unit.

---
+++ json/tata.php
@@ -0,0 +1,12 @@
+<?php
+
+$msg = array(
+    "text"=>"Hi there Victor!",
+    "date"=>"Monday 21th Feb 2010"
+    );
+
+$mymsg = json_encode($msg);
+
+echo $mymsg;
+
+?>
---
+++ json/testjson.php
@@ -0,0 +1,12 @@
+<?php
+
+$msg = array(
+    "date"=>"23rd May 2010",
+    "text"=>"sfjkdls;af"
+);
+
+$x = json_encode($msg);
+
+echo $x;
+
+?>
